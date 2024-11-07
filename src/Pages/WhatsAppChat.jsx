b45b86f1313717f987532ddb476c3ace226217af
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-11-07 08:47:49 +0600

    complete project

--- src/Pages/WhatsAppChat.jsx
+++ src/Pages/WhatsAppChat.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Header from '../Components/Header';
+import Profile from '../Components/Profile';
+import CommentBox from '../Components/CommentBox';
+import MessageBoxFooter from '../Components/MessageBoxFooter';
 
 const WhatsAppChat = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+
+            <Header />
+            <Profile />
+            <CommentBox />
+            <MessageBoxFooter />
+        </>
     );
 };
 
