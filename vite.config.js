1ddbd4ebe47730293b98aa80f255057309369c15
tanvirkhan00 <kmtanvir1111@gmail.com>
2024-11-07 17:05:59 +0600

    ggg

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Update with the correct path, typically matching your GitHub repository name
   plugins: [react()],
 })
